69fce6540c24d1936c61eeb6ef005ce8deeb76a4
mehdi1361 <mhd.mosavi@gmail.com>
2018-02-10 13:54:55 +0330

    del python3.6

--- py36venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/mehdi/python-project/Divine_Server_Python/py36venv/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
