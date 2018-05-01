cb9e1b3cf03b78a78544c04aa6e12af8292aa166
mehdi1361 <mhd.mosavi@gmail.com>
2018-05-01 13:23:14 +0430

    test print

--- common/utils.py
+++ common/utils.py
@@ -151,7 +151,8 @@ class ChestGenerate:
         unit_list = list(Unit.objects.filter(unlock=True).values_list('id', flat=True))
         unit_index = random.choice(unit_list)
         unit = Unit.objects.get(pk=unit_index)
-	print unit
+
+        print(unit)
 
         data = {
             "unit": str(unit),
