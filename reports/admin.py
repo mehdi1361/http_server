9a299cd45b76367c509b3564aff823a23e5e6da7
mehdi1361 <mhd.mosavi@gmail.com>
2018-06-20 14:32:04 +0430

    resolve conflict

--- reports/admin.py
+++ reports/admin.py
@@ -14,9 +14,6 @@ class BattleAdmin(admin.ModelAdmin):
     list_display = ['player_1', 'player_1_bot', 'player_2', 'player_2_bot', 'status', 'battle_id',
                     'created_date', 'updated_date']
     inlines = [TransactionInline, ]
-
-<<<<<<< HEAD
     list_filter = ['created_date', ]
 
-=======
->>>>>>> 9c918644d14535bc8ac1b7e461cd7eb4fef0eb95
+
