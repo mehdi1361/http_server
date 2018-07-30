d43ef49244c748cc686fd6d07d6344f94197ba22
mehdi1361 <mhd.mosavi@gmail.com>
2018-07-30 17:19:55 +0430

    change ctm logic
    (cherry picked from commit 90ff0ba)

--- system_settings/admin.py
+++ system_settings/admin.py
@@ -2,5 +2,61 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import CTM, CTMUnit, CTMHero
 
-# Register your models here.
+
+class CTMUnitInline(admin.StackedInline):
+    list_display = [
+        'unit',
+        'enable'
+    ]
+    readonly_fields = ['unit', 'params']
+    model = CTMUnit
+
+
+class CTMHeroInline(admin.StackedInline):
+    list_display = [
+        'hero',
+        'enable'
+    ]
+    readonly_fields = ['hero', 'params']
+    model = CTMHero
+
+
+@admin.register(CTM)
+class CTMAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'chest_type',
+        'league',
+        'min_coin',
+        'max_coin',
+        'min_gem',
+        'max_gem',
+        'min_troop',
+        'max_troop',
+        'chance_troop',
+        'min_hero',
+        'max_hero',
+        'chance_hero',
+        'total',
+        'card_try',
+    ]
+    list_editable = [
+        'chest_type',
+        'league',
+        'min_coin',
+        'max_coin',
+        'min_gem',
+        'max_gem',
+        'min_troop',
+        'max_troop',
+        'chance_troop',
+        'min_hero',
+        'max_hero',
+        'chance_hero',
+        'total',
+        'card_try'
+    ]
+
+    inlines = (CTMUnitInline, CTMHeroInline)
