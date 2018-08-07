ce59ade6807a738c9a2906e4fd0cb37de747886c
mehdi1361 <mhd.mosavi@gmail.com>
2018-08-07 11:15:53 +0430

    add ctm test page
    (cherry picked from commit 3100fec)

--- system_settings/views.py
+++ system_settings/views.py
@@ -2,9 +2,38 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import CtmTestForm
+from objects.models import League
+from common.utils import CtmChestGenerate
+
 
 # Create your views here.
 
 
 def test_ctm(request):
-    return render(request, 'admin/test_ctm.html')
+    lst_result = []
+    len_header = 0
+    if request.method == 'POST':
+        form = CtmTestForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            league = League.objects.get(pk=int(cd['league']))
+
+            for i in range(1, int(cd['count'])):
+                chest = CtmChestGenerate(request.user, chest_type=str(cd['chest']), league=league)
+                result = chest.generate_chest()
+                lst_result.append(result)
+                len_header = len(result['units'])
+
+    else:
+        form = CtmTestForm()
+
+    return render(
+        request, 'admin/test_ctm.html',
+        {
+            'form': form,
+            'len_header': len_header,
+            'result': lst_result
+        }
+    )
